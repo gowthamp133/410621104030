8adb5fa5ae5fe477b729dd1de004c25fbb8d06bd
Gowtham <114680322+gowthamp133@users.noreply.github.com>
2024-07-10 11:41:04 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.css';
+import logo from './logo.svg';
+import './App.css';
+import Average from "./calc"
+
+function App() {
+  return (
+    <div className="App">
+      <Average/>
+    </div>
+  );
+}
+
+export default App;
